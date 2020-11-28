5a18483f31a52acbd1528aacf1020162ad5da72a
Nabin Khanal <nabin5778@gmail.com>
2020-11-28 11:15:27 +0545

    solved cudnn error

--- yolo.py
+++ yolo.py
@@ -16,6 +16,20 @@ from yolo4.utils import letterbox_image
 import os
 from keras.utils import multi_gpu_model
 
+def solve_cudnn_error():
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    if gpus:
+        try:
+            # Currently, memory growth needs to be the same across GPUs
+            for gpu in gpus:
+                tf.config.experimental.set_memory_growth(gpu, True)
+            logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+            print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+        except RuntimeError as e:
+            # Memory growth must be set before GPUs have been initialized
+            print(e)
+solve_cudnn_error()
+
 class YOLO(object):
     def __init__(self):
         self.model_path = 'model_data/yolo4.h5'
